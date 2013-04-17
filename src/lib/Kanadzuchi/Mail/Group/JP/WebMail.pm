250dd942f2fceb42a1fdd704a2d291b2d322a75c
azumakuniyuki <azumakuniyuki@gmail.com>
2013-04-17 13:15:56 +0900

    Update domains of infoseek and nifty

--- src/lib/Kanadzuchi/Mail/Group/JP/WebMail.pm
+++ src/lib/Kanadzuchi/Mail/Group/JP/WebMail.pm
@@ -1,4 +1,4 @@
-# $Id: WebMail.pm,v 1.8.2.1 2013/04/15 04:20:53 ak Exp $
+# $Id: WebMail.pm,v 1.8.2.2 2013/04/15 09:33:32 ak Exp $
 # Copyright (C) 2010,2013 Cubicroot Co. Ltd.
 # Kanadzuchi::Mail::Group::JP::
                                                    
@@ -28,23 +28,25 @@ sub nominisexemplaria
 			qr{\Aauone[.]jp\z},
 		],
 		'goo' => [
-			# goo mail, http://mail.goo.ne.jp/goomail/index.ghtml
+			# goo mail, http://mail.goo.ne.jp/index.html
 			qr{\Amail[.]goo[.]ne[.]jp\z},
 			qr{\Agoo[.]jp\z},
 		],
 		'infoseek' => [
 			# http://infoseek.jp/ < 50MB...
 			qr{\Ainfoseek[.]jp\z},
+			qr{\Arakuten[.]com\z},
 		],
 		'livedoor' => [
 			# livedoor mail(Gmail) http://mail.livedoor.com/
-			qr{\Alivedoor[.]com\z},
+			qr{\Alivedoor[.]com\z},	# Until Oct 31, 2013
 		],
 		'nifty' => [
 			# http://www.nifty.com/
 			qr{\Anifty[.]com\z},
-			qr{\Anifmail[.]jp\z},	# Until Sep 30, 2010
-			qr{\A(?:mb|sp).+[.]nifty[.]com\z},
+			qr{\Anifmail[.]jp\z},			# Until Sep 30, 2010
+			qr{\A(?:mb|sp).+[.]nifty[.]com\z},	# http://www.nifty.com/mail/mailaccount/service.htm
+			qr{\A[0-9a-z]+[.]nifty[.]jp\z},		# http://www.nifty.com/mail/plus/index.htm
 
 			# http://www.nifty.com/mail/sanrio/domainlist.htm
 			qr{\A(?:kitty|x[-]o|mymelody|usahana|mimmy|kikilala|charmmy|cinnamonroll)[.]jp\z},
